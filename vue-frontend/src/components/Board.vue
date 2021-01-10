e20032db64b36250ae190c0c65712596f8f6e172
Faith Ege <faithege@hotmail.co.uk>
2021-01-10 20:25:28 +0000

    Start adding a vue front end

---
+++ vue-frontend/src/components/Board.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>Connect Four</h1>
+    <div v-for="(row, index) in board" :key="index">
+      <Row :row="row"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Row from './Row.vue'
+
+export default {
+  name: 'Board',
+  components: {
+    Row
+  },
+  props: {},
+  data: function () {
+    return {
+      board: [ 
+        [".",".",".",".",".",".","."],
+        [".",".",".",".",".",".","."],
+        [".",".",".",".",".",".","."],
+        [".",".",".",".",".",".","."],
+        [".",".",".",".",".",".","."],
+        [".",".",".",".",".",".","."]
+        ]
+    }
+  },
+  methods:{
+    generateRows: function() {
+      // const rowStrings = this.board.map(row => row.join(' '))
+      const rowStrings = this.board.map(row => 
+                                          row.map(place => this.getPlaceImageUrl(place)))
+      return rowStrings 
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ vue-frontend/src/components/Row.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <span v-for="(place, index) in row" :key="index">
+      <!-- <img :src="getPlaceImageUrl(place)"> -->
+      {{ getPlaceImageUrl(place) }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+  props: {
+    row: Array
+  },
+  data: function () {
+    return {}
+  },
+  methods: {
+    getPlaceImageUrl: function(place){
+      let url
+
+      switch (place) {
+        case 'r':
+          url = './src/assets/red.png';
+          break;
+        case 'y':
+          url = './src/assets/yellow.jpeg';
+          break;
+        default:
+          url = '../src/assets/logo.png';
+        }
+      return url
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
