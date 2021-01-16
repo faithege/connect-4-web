944eedd6833a0106b8765ef5af357937f6988df0
Faith Ege <faithege@hotmail.co.uk>
2021-01-16 18:30:08 +0000

    Display board images

--- vue-frontend/src/components/Row.vue
+++ vue-frontend/src/components/Row.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <span v-for="(place, index) in row" :key="index">
-      <!-- <img :src="getPlaceImageUrl(place)"> -->
-      {{ getPlaceImageUrl(place) }}
+      <img class="placeImage" src="../assets/red.png" v-if="place == 'r'">
+      <img class="placeImage" src="../assets/yellow.jpeg" v-else-if="place == 'y'">
+      <img class="placeImage" src="../assets/square-tile.png" v-else>
+      <!-- <img :src="require(getPlaceImageUrl(place))" :key="index" > -->
     </span>
   </div>
 </template>
@@ -17,25 +19,13 @@ export default {
     return {}
   },
   methods: {
-    getPlaceImageUrl: function(place){
-      let url
-
-      switch (place) {
-        case 'r':
-          url = './src/assets/red.png';
-          break;
-        case 'y':
-          url = './src/assets/yellow.jpeg';
-          break;
-        default:
-          url = '../src/assets/logo.png';
-        }
-      return url
-    }
   },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .placeImage {
+    max-width:100px
+  }
 </style>
