96cfd5436dbd545ce3d70d3df6b6f06c81a2c242
Faith Ege <faithege@hotmail.co.uk>
2021-02-13 21:54:58 +0000

    Add vue-router

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+const routes = [
+  { path: '/:gameId/:playerId', component: App }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
