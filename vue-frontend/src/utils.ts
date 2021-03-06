97f4d98e2044aab911fbeed5a60e33f717bc1c58
Faith Ege <faithege@hotmail.co.uk>
2021-03-06 18:49:23 +0000

    Remove duplicated models and utils files from frontend project

--- vue-frontend/src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function generateId() :GameId {
-    const idLength = 32 //how long we want the game id -> the bigger the less liklihood of collision
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    const result = Array(idLength).fill(undefined)
-                                    .map(_ => characters.charAt(Math.floor(Math.random() * charactersLength)))
-                                    .join('')
-    return result;
-  }
