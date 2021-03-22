6c3cb4928ccdd43c678927f35cf236b5cc532a96
Faith Ege <faithege@hotmail.co.uk>
2021-03-22 23:18:30 +0000

    Create new link for second player when initial player creates game

--- vue-frontend/src/domain/api.ts
+++ vue-frontend/src/domain/api.ts
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
 export default {
-    createNewGame: async function() {
-        try {
-            const response = await axios.post('https://ikx385nw43.execute-api.eu-west-1.amazonaws.com/dev/new');
-            console.log(response);
-          } catch (error) {
-            console.error(error);
-          }
+    createNewGame: function() : Promise<any>{
+        return  axios.post('https://ikx385nw43.execute-api.eu-west-1.amazonaws.com/dev/new');
     },
 }
--- vue-frontend/src/pages/NewGame.vue
+++ vue-frontend/src/pages/NewGame.vue
@@ -7,7 +7,14 @@
       </b-row>
       <b-row>
         <b-col cols="2" offset="5">
-          <b-button size="lg" @click="handleClick()">Start Game</b-button>
+          <b-button size="lg" @click="handleClick()">Start New Game</b-button>
+        </b-col>
+      </b-row>
+      <b-row 
+        v-if="shareableUrl">
+        <b-col cols="6" offset="3">
+          <label for="nextPlayerUrl">To get started, share this link with the other player:</label>
+          <b-form-input id="nextPlayerUrl" v-model="shareableUrl"></b-form-input>
         </b-col>
       </b-row>
   </Section>
@@ -19,12 +26,30 @@ import api from '@/domain/api.ts'
 export default {
   name: 'NewGamePage',
   data: function() {
-    return {}
+    return {
+      shareableUrl: null
+    }
   },
   methods: {
-    handleClick() {
-      console.log("Button clicked")
-      api.createNewGame()
+    async handleClick() {
+      try {
+            const response = await api.createNewGame();
+            this.gameId = response.data.gameId;
+            this.playerId = response.data.currentPlayer;
+            this.nextPlayerId = this.generateNextPlayer(this.playerId);
+            this.shareableUrl = `${window.location.origin}/${this.gameId}/${this.nextPlayerId}`;
+            console.log(response);
+          } catch (error) {
+            console.error(error);
+          }
+    },
+    generateNextPlayer(playerId){
+      if (playerId == 'r') {
+        return 'y'
+      }
+      else if (playerId === 'y') {
+        return 'r'
+      }
     }
   },
   
