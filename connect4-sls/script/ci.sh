ce65f67fa14cf40abe83b41c1b3e9438ac576264
Faith Ege <faithege@hotmail.co.uk>
2021-03-23 20:27:23 +0000

    Correct directory location

--- connect4-sls/script/ci.sh
+++ connect4-sls/script/ci.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e #very important in ci, install cna fail for example and we would want to stop all further code
 
+# Reliably get the directory containing this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ${DIR}/..
 npm install
 #compile, test, ship
 npm run compile #we might be able to get rid of this if serveless deploy also compiles
