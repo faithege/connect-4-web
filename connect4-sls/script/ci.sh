60f9ad121d6845bd058d6967e3821e5ea9a1280f
Faith Ege <faithege@hotmail.co.uk>
2021-03-23 20:42:35 +0000

    Make sure serverless installed

--- connect4-sls/script/ci.sh
+++ connect4-sls/script/ci.sh
@@ -5,12 +5,12 @@ set -e #very important in ci, install cna fail for example and we would want to 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${DIR}/..
-serverless config credentials \
+npm install
+npm run serverless -- config credentials \
  --provider aws \
  --key ${AWS_ACCESS_KEY_ID} \
  --secret ${AWS_SECRET_ACCESS_KEY} \
  --profile cbf
-npm install
 #compile, test, ship
 npm run compile #we might be able to get rid of this if serveless deploy also compiles
 npm run deploy -- --stage prod #credentials needed, add stage arguments
