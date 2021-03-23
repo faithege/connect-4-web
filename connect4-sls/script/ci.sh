2e068c1e85cb02284a57f06d9aefa486976919f4
Faith Ege <faithege@hotmail.co.uk>
2021-03-23 20:35:00 +0000

    Add aws profile to deployment

--- connect4-sls/script/ci.sh
+++ connect4-sls/script/ci.sh
@@ -5,6 +5,11 @@ set -e #very important in ci, install cna fail for example and we would want to 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${DIR}/..
+serverless config credentials \
+ --provider aws \
+ --key ${AWS_ACCESS_KEY_ID} \
+ --secret ${AWS_SECRET_ACCESS_KEY} \
+ --profile cbf
 npm install
 #compile, test, ship
 npm run compile #we might be able to get rid of this if serveless deploy also compiles
