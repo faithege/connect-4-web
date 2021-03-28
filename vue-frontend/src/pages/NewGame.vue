206e85751fca831354458990e44501621a2b465d
Faith Ege <faithege@hotmail.co.uk>
2021-03-28 20:25:08 +0100

    Add copy to clipboard button

--- vue-frontend/src/pages/NewGame.vue
+++ vue-frontend/src/pages/NewGame.vue
@@ -13,8 +13,16 @@
       <b-row 
         v-if="shareableUrl">
         <b-col cols="6" offset="3">
-          <label for="nextPlayerUrl">To get started, share this link with the other player:</label>
+          <b-input-group prepend="Share with your opponent" class="mt-3">
           <b-form-input id="nextPlayerUrl" v-model="shareableUrl"></b-form-input>
+          <b-input-group-append>
+            <button type="button" @click="handleCopy">
+              <span class="far fa-copy" />
+            </button>
+          </b-input-group-append>
+        </b-input-group>
+          <p v-if="copySucceeded">Copied!</p>
+          <p v-else>Press CTRL+C to copy.</p>
         </b-col>
       </b-row>
   </Section>
@@ -27,13 +35,15 @@ export default {
   name: 'NewGamePage',
   data: function() {
     return {
-      shareableUrl: null
+      shareableUrl: null,
+      copySucceeded: null
     }
   },
   methods: {
     async handleClick() {
       try {
             const response = await api.createNewGame();
+            this.copySucceeded = false
             this.gameId = response.data.gameId;
             this.playerId = response.data.currentPlayer;
             this.nextPlayerId = this.generateNextPlayer(this.playerId);
@@ -43,6 +53,10 @@ export default {
             console.error(error);
           }
     },
+    handleCopy: function () {
+        this.$copyText(this.shareableUrl)
+        this.copySucceeded = true
+      },
     generateNextPlayer(playerId){
       if (playerId == 'r') {
         return 'y'
