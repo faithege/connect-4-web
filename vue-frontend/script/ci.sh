ea7d75fee241be3a66a0a235bfc75ee33d91eb57
Faith Ege <faithege@hotmail.co.uk>
2021-04-06 20:18:58 +0100

    Update ci bash script to create dist directory + cleanup

--- vue-frontend/script/ci.sh
+++ vue-frontend/script/ci.sh
@@ -4,19 +4,22 @@ set -e # very important in ci, install can fail for example and we would want to
 # Reliably get the directory containing this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd ${DIR}/..
+DIST="${DIR}/../dist"
 
 # clean up existing dist file first > if don't will become additive (not best practice)
-rm -r ${DIR}/../dist
+rm -r ${DIST}
 
+mkdir -p ${DIST} #-p stops complaints if it already exists (side effect, actually means create parents)
+
+cd ${DIR}/..
 npm install
 
 #compile, test
 npm run build-prod # if unsuccessful will blow up
 
 # for SPA we need to copy the index.html file and rename as 404.html
-cp ${DIR}/../dist/index.html ${DIR}/../dist/404.html # be explicit
+cp ${DIST}/index.html ${DIST}/404.html # be explicit
 
-echo "c4.faithege.dev" > ${DIR}/../dist/CNAME
+echo "c4.faithege.dev" > ${DIST}/CNAME
 
 
